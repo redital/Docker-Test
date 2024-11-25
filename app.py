fd58d2c485a9b783776b0eedb069b36b56b6c786
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2024-11-25 04:41:07 +0100

    proviamo va

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def hello():
     num = module.print_random()
     count = 1
     print("questo me lo stampi?")
-    return f'Hello World! I have been seen {count} times.\n Random generated number {num}'
+    return f'Hello World! I have been seen {count} times.\n Random generated number {num}\n NO WAY FUNZIONA'
