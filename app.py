9ad340d25ffc62724a377dbec05c8629490759b2
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2024-11-25 11:06:43 +0100

    add print

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ import module
 
 app = Flask(__name__)
 
+print("ECCOMI QUA")
 
 @app.route('/')
 def hello():
+    print("ho ricevuto la chiamata")
     num = module.print_random()
     count = 1
+    print("adesso ritorno")
     return f'Hello World! I have been seen {count} times.\n Random generated number {num}\n NO WAY FUNZIONA'
