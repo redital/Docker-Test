adc9e8dc4176a737d524c83c683e000c2648deba
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2024-11-25 04:22:30 +0100

    get rid of that

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
 import time
 
-import redis
 from flask import Flask
 
 import module
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
 @app.route('/')
 def hello():
     num = module.print_random()
-    count = get_hit_count()
+    count = 1
     return f'Hello World! I have been seen {count} times.\n Random generated number {num}'
