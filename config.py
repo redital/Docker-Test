417bed5c12484a4e8b08ea80a23ef1c9129022cd
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2024-11-29 10:24:53 +0100

    vgyhuk

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ flask_app_config = {
     "port": os.environ.get("FLASK_PORT", 5000),
     "ssl_context": os.environ.get("SSL_CONTEX","adhoc"),
     "allow_unsafe_werkzeug": os.environ.get("UNSAFE_WERKZEUG",True),
+    "cors_allowed_origins":"*", 
+    "engineio_logger":True
 }
 
 
